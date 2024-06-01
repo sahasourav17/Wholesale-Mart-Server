489fd92eecbdfde5eba38fb7325f2c71d54b30e3
sahasourav17 <sahasourav1170@gmail.com>
2024-06-01 08:16:45 +0600

    refactor: payment controller

--- controller/payment/paymentController.js
+++ controller/payment/paymentController.js
@@ -5,42 +5,49 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const createPayment = async (req, res) => {
   const { token, shippingInfo, cartItems, user } = req.body;
 
-  //total calcutation
-  const total = cartItems.reduce(
-    (total, item) => total + item.price * item.quantity,
-    0
-  );
+  // Calculate total amount in cents
+  const totalAmount =
+    cartItems.reduce((total, item) => total + item.price * item.quantity, 0) *
+    100;
 
   try {
     // Use the Stripe token to create a charge
     const charge = await stripe.charges.create({
-      amount: total, // Amount in cents
+      amount: totalAmount,
       currency: "usd",
       source: token.id,
       description: "Example Charge",
     });
 
-    // Save shipping info and transaction ID to MongoDB
-    if (charge.id) {
-      const order = new Order({
-        user: user._id,
-        transactionId: charge.id,
-        items: cartItems,
-        totalAmount: total,
-        shippingInfo: shippingInfo,
-      });
-      const result = await order.save();
-      console.log(result);
-      if (result) {
-        res.json({ success: true, charge });
-      } else {
-        res.status(500).json({ error: "Payment failed" });
-      }
-    } else {
-      res.status(500).json({ error: "Payment failed" });
+    if (!charge.id) {
+      return res
+        .status(500)
+        .json({ error: "Payment processing failed. No charge ID returned." });
     }
+
+    // Save order information to MongoDB
+    const order = new Order({
+      user: user._id,
+      transactionId: charge.id,
+      items: cartItems,
+      totalAmount: totalAmount / 100, // Convert back to dollars for saving
+      shippingInfo: shippingInfo,
+    });
+    const savedOrder = await order.save();
+
+    if (!savedOrder) {
+      return res
+        .status(500)
+        .json({ error: "Failed to save order to the database." });
+    }
+
+    res.status(200).json({ success: true, charge });
   } catch (err) {
-    res.status(500).json({ error: "Payment failed" });
+    console.error(err);
+    res.status(500).json({
+      error: "An error occurred during payment processing.",
+      details: err.message,
+    });
   }
 };
 
